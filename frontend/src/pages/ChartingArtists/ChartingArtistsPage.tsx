a0817b4095fcfc95129f6c95ce290a54edb72b3d
alexpotts <alex.potts@universalstore.com.au>
2024-03-18 00:50:43 +1000

    feat(frontend): Charting Artists page
    - React component competed for charting artists - contacts backend for relevant artists and provides links to them

--- frontend/src/pages/ChartingArtists/ChartingArtistsPage.tsx
+++ frontend/src/pages/ChartingArtists/ChartingArtistsPage.tsx
@@ -1,14 +1,22 @@
 import React, {useEffect, useState} from "react";
 import {Artist} from "../../../../shared/types/music";
 import axios from "axios";
+import {Link, useParams} from "react-router-dom";
 
 const ChartingArtistsPage: React.FC = () => {
     const [artists, setArtists] = useState<Artist[]>([]);
+    const {country} = useParams<{ country: string }>();
 
+    /**
+     * Fetch the top charting artists for the given country
+     * and provide a link to their albums
+     */
     useEffect(() => {
         const fetchArtists = async () => {
             try {
-                const artists = await axios.get('http://localhost:3001/api/music/charting-artists');
+                const artists = await axios.get('http://localhost:3001/api/music/charting-artists', {params: {country}});
+                console.log('artists', artists);
+                setArtists(artists.data);
             } catch (error: any) {
                 console.error(error);
                 setArtists([] as Artist[]);
@@ -16,13 +24,19 @@ const ChartingArtistsPage: React.FC = () => {
         }
 
         fetchArtists();
-    }, []);
+    }, [country]);
 
     return (
         <div>
-            <h1>Top Charting Artists</h1>
+            <h1>Top Charting Artists in {country}</h1>
             <ul>
-                {artists.map(artist => <li key={artist.artist_id}>{artist.artist_name}</li>)}
+                {artists.map(artist => {
+                    return <li key={artist?.artist?.artist_id}>
+                        <Link to={`artist-albums/${artist?.artist?.artist_id}`}>
+                            {artist?.artist?.artist_name}
+                        </Link>
+                    </li>
+                })}
             </ul>
         </div>
     );
